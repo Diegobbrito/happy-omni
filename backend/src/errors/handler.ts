5842bfd39ab728de0b06489850dacfa7220bbfb2
Diego Brito <dibbrito@gmail.com>
2020-10-14 00:39:27 -0300

    add errors handler

---
+++ backend/src/errors/handler.ts
@@ -0,0 +1,8 @@
+import {ErrorRequestHandler, response} from 'express'
+const errorHandler: ErrorRequestHandler = (error, request, response, next) =>{
+    console.error(error);
+
+    return response.status(500).json({message: 'Internal server error'})
+};
+
+export default errorHandler;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import path from 'path'
+import 'express-async-errors'
 import './database/connection';
 import routes from './routes';
 
@@ -6,5 +8,6 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 
 app.listen(3333)
