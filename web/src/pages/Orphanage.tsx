7e5f59c2c44b953b0dd1201d7f8e3e511a3415ab
Diego Brito <dibbrito@gmail.com>
2020-10-18 13:49:45 -0300

    Initial mobile

--- web/src/pages/Orphanage.tsx
+++ web/src/pages/Orphanage.tsx
@@ -91,7 +91,7 @@ export default function Orphanage() {
                 {/* <TileLayer
                   // url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
                   
-                /> */}
+                /> */} 
                 <Marker
                   interactive={false}
                   icon={mapIcon}
